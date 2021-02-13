b45795ffcc9e88c5d931fe6b34d555c75b843ea2
chandra <chandra.chaganti@hotmail.com>
2021-02-13 07:37:43 +0100

    sm8250-common: op camera

--- common.mk
+++ common.mk
@@ -186,7 +186,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
@@ -301,6 +300,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     libhwbinder.vendor
+
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
